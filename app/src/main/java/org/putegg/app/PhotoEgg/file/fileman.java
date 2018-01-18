b473997b6f2acaee88d2bbd1d1a21e41ee347421
PutEgg <yaoshou@outlook.com>
2018-01-18 18:38:37 +0800

    自动创建新用户个人根目录

---
+++ app/src/main/java/org/putegg/app/PhotoEgg/file/fileman.java
@@ -0,0 +1,37 @@
+package org.putegg.app.PhotoEgg.file;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by hp on 2018/1/18.
+ * 用于文件操作
+ */
+
+public class fileman {
+
+    public void fileman(){
+
+    }
+
+    public void createUserHome(String uname){
+        //创建新用户个人根目录
+        File f = new File("/data/data/org.putegg.app.PhotoEgg/users/"+uname+"/");
+            f.mkdirs();
+    }
+
+    public String getUserHomeString(String uname){
+        String a ="/data/data/org.putegg.app.PhotoEgg/users/"+uname+"/";
+        return a;
+    }
+
+    public void createFile(String userdir,String filename) throws IOException {
+        File file = new File(userdir,filename);
+        file.createNewFile();
+    }
+
+}
--- app/src/main/java/org/putegg/app/PhotoEgg/view/hub.java
+++ app/src/main/java/org/putegg/app/PhotoEgg/view/hub.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.putegg.app.PhotoEgg.file.fileman;
 import org.putegg.app.PhotoEgg.R;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 /**
  * Created by PutEgg on 2018/1/17.
@@ -27,6 +32,30 @@ public class hub extends Activity{
         banner.setText(Html.fromHtml(user));
         banner.setMovementMethod(LinkMovementMethod.getInstance());
 
+        fileman fileman = new fileman();
+        String userHomeString = fileman.getUserHomeString(user);
+        File userhome = new File(userHomeString);
+        if (!userhome.exists()){
+            fileman.createUserHome(user);
+            bugpoint("初次登陆，成功创建用户文件夹");
+        }
+        else if (userhome.exists()){
+            bugpoint("用户文件夹已找到");
+        }
+        try {
+            fileman.createFile(userHomeString,user+".txt");
+            bugpoint("成功创建测试文件");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void bugpoint(String text){
+        Toast.makeText(getApplicationContext(),
+                text,
+                Toast.LENGTH_SHORT).show();
     }
 
 }
